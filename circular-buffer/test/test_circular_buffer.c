000ddac704160468b40bda09e9001b8a2a502751
smartkeyerror <smartkeyerror@gmail.com>
2020-10-13 17:22:23 +0800

    :construction: Add circular-buffer/test

---
+++ circular-buffer/test/test_circular_buffer.c
@@ -0,0 +1,15 @@
+#include "vendor/unity.h"
+
+#include "../src/circular_buffer.h"
+
+void setUp(void) {}
+
+
+void tearDown(void) {}
+
+
+int main(){
+    UnityBegin("test/test_circular_buffer.c");
+
+    return UnityEnd();
+}
