f6bc47a9ca7a855c8b056e3f894ca905dc3ed93f
smartkeyerror <smartkeyerror@gmail.com>
2020-10-27 16:03:43 +0800

    :sparkles: Add hash table implementations

--- src/hash-table/src/hashtable.c
+++ src/hash-table/src/hashtable.c
@@ -6,7 +6,8 @@
 
 #define SUCCESS 0
 #define FAIL -1
-#define INIT_CAPACITY 1024
+
+#define INIT_CAPACITY 16
 
 /*
  * DJB algorithm from Daniel J. Bernstein
@@ -39,6 +40,7 @@ HashTable *new_hash_table(void) {
 
     hash_table->size = 0;
     hash_table->table = table;
+    hash_table->load_factor = 0.75;
     hash_table->capacity = INIT_CAPACITY;
 
     // 将所有指针初始化为NULL
@@ -48,6 +50,10 @@ HashTable *new_hash_table(void) {
 
     return hash_table;
 }
+
+static int expand_hash_table(HashTable *hash_table);
+
+static void rehash(HashTable *hash_table, HashElement **new_table, unsigned int new_capacity);
 
 int hash_table_put(HashTable *hash_table, char *key, void *value) {
     unsigned int index = hash_calc(key, strlen(key)) % hash_table->capacity;
@@ -78,8 +84,61 @@ int hash_table_put(HashTable *hash_table, char *key, void *value) {
     hash_table->table[index] = new_element;
     hash_table->size++;
 
+    // 计算哈希表实际负载，若超过0.75则进行扩容(非线程安全)
+    if (hash_table->size > hash_table->capacity * hash_table->load_factor) {
+        int result = expand_hash_table(hash_table);
+        return result;
+    }
+
     return SUCCESS;
 }
+
+static int expand_hash_table(HashTable *hash_table) {
+    /*
+     * 扩容规则: 
+     *  当 capacity 小于 1024 时，每次扩容增长 1 倍;
+     *  当 capacity 大于 1024 时，每次扩容增长 0.25 倍。
+     */
+    unsigned int new_capacity;
+
+    if (hash_table->capacity < 1024)
+        new_capacity = hash_table->capacity * 2;
+    else
+        new_capacity = hash_table->capacity * 1.25;
+    
+    // 申请内存
+    HashElement **new_table = (HashElement **)malloc(new_capacity * sizeof(HashElement *));
+
+    if (new_table == NULL)
+        return FAIL;
+    
+    rehash(hash_table, new_table, new_capacity);
+
+    return SUCCESS;
+}
+
+static void rehash(HashTable *hash_table, HashElement **new_table, unsigned int new_capacity) {
+    for (unsigned int i = 0; i < hash_table->capacity; i++) {
+        HashElement *current = hash_table->table[i];
+
+        while (current != NULL) {
+            HashElement *element = current;
+
+            // 保留当前元素的下一个元素，避免原哈希表链表出现断裂
+            current = current->next;
+
+            unsigned int index = hash_calc(element->key, strlen(element->key)) % new_capacity;
+            element->next = new_table[index];
+            new_table[index] = element;
+        }
+    }
+    // 释放原有table内存空间
+    free(hash_table->table);
+
+    hash_table->table = new_table;
+    hash_table->capacity = new_capacity;
+}
+
 
 int hash_table_remove(HashTable *hash_table, char *key) {
     unsigned int index = hash_calc(key, strlen(key)) % hash_table->capacity;
@@ -115,5 +174,32 @@ void *hash_table_get(HashTable *hash_table, char *key) {
         current = current->next;
     }
     return NULL;
+}
+
+void list_hash_table_keys(HashTable *hash_table, void **buffer) {
+    int buffer_index = 0;
+    for (unsigned int i = 0; i < hash_table->capacity; i++) {
+        HashElement *current = hash_table->table[i];
+
+        while (current != NULL) {
+            buffer[buffer_index++] = current->key;
+            current = current->next;
+        }
+    }
+}
+
+void free_hash_table(HashTable *hash_table) {
+    for (unsigned int i = 0; i < hash_table->capacity; i++) {
+        HashElement *current = hash_table->table[i];
+
+        while (current != NULL) {
+            HashElement *element = current;
+            current = current->next;
+            free(element);
+        }
+    }
+    free(hash_table->table);
+    free(hash_table);
+    hash_table = NULL;
 }
 
--- src/hash-table/src/hashtable.h
+++ src/hash-table/src/hashtable.h
@@ -13,8 +13,19 @@ typedef struct hash_table {
     unsigned int size;              // hashtable实际存储元素数量
     unsigned int capacity;          // 初始化元素数量
     HashElement **table;            // 指针实现的数组，便于扩容
+    double load_factor;             // 负载因子，默认为0.75(参考JDK实现)
 } HashTable;
 
+HashTable *new_hash_table(void);
 
+int hash_table_put(HashTable *hash_table, char *key, void *value);
+
+int hash_table_remove(HashTable *hash_table, char *key);
+
+void *hash_table_get(HashTable *hash_table, char *key);
+
+void list_hash_table_keys(HashTable *hash_table, void **buffer);
+
+void free_hash_table(HashTable *hash_table);
 
 #endif
