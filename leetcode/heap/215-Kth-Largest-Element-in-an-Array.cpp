3a1f52184c3628e39fda98c91b4eb32d7021cb34
smartkeyerror <smartkeyerror@gmail.com>
2020-11-28 21:08:51 +0800

    :zap: improve 'Kth Largest Element in an Array' performance

--- leetcode/heap/215-Kth-Largest-Element-in-an-Array.cpp
+++ leetcode/heap/215-Kth-Largest-Element-in-an-Array.cpp
@@ -1,6 +1,8 @@
 
 #include <vector>
 #include <queue>
+#include <ctime>
+#include <algorithm>
 
 using namespace std;
 
@@ -44,9 +46,53 @@ private:
 
         return minHeap.top();
     }
+
+    int __partition(vector<int> &nums, int left, int right) {
+        // 两路分片，只需要找到 < v 和 > v 的分界点即可
+
+        // 随机选取锚定点，并将其置于当前子数组的首位
+        swap(nums[left], nums[left+rand()%(right-left+1)]);
+
+        int demarcation = left, v = nums[left];
+
+        // nums[left+1...demarcation] < v, nums[demarcation+1, right] >= v
+        for (int i = left + 1; i <= right; i++) {
+            if (nums[i] < v) {
+                demarcation ++;
+                swap(nums[i], nums[demarcation]);
+            }
+        }
+        swap(nums[left], nums[demarcation]);
+        return demarcation;
+    }
+
+    int __selection(vector<int>& nums, int left, int right, int k) {
+        if (left == right) return nums[left];
+
+        int pos = __partition(nums, left, right);
+
+        if (nums.size() - k == pos)
+            return nums[pos];
+        else if (nums.size() - k > pos) 
+            // 此时，我们应该去右边寻找
+            return __selection(nums, pos + 1, right, k);
+        else
+            return __selection(nums, left, pos - 1, k);
+        
+    }
+
+    // 最后，我们可以借助两路快排的方式来完成
+    // Runtime: 16 ms, faster than 81.83% of C++ online submissions for Kth Largest Element in an Array.
+    // Memory Usage: 10.3 MB, less than 88.36% of C++ online submissions for Kth Largest Element in an Array.
+    int findKthLargestUsePartition(vector<int>& nums, int k) {
+        srand(time(NULL));
+        return __selection(nums, 0, nums.size() - 1, k);
+    }
+
 public:
     int findKthLargest(vector<int>& nums, int k) {
         // return findKthLargestUseHeap(nums, k);
-        return findKthLargestUseHeapOptimization(nums, k);
+        // return findKthLargestUseHeapOptimization(nums, k);
+        return findKthLargestUsePartition(nums, k);
     }
 };
