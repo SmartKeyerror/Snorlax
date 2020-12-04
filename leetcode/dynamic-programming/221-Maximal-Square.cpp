fe34b71a497496cfce06623589cd2cbd9cd92a95
smartkeyerror <smartkeyerror@gmail.com>
2020-12-04 14:02:10 +0800

    :art:

--- leetcode/dynamic-programming/221-Maximal-Square.cpp
+++ leetcode/dynamic-programming/221-Maximal-Square.cpp
@@ -23,7 +23,7 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int res = 0;
-        vector<vector<int>> memory = vector<vector<int>>(matrix.size(), vector<int>(matrix[0].size(), 0));
+        vector<vector<int>> memory(matrix.size(), vector<int>(matrix[0].size(), 0));
         
         for (int i = 0; i < matrix.size(); i++) {
             for (int j = 0; j < matrix[0].size(); j++) {
