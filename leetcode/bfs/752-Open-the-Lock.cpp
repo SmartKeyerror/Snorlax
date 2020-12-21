60b7e26fee9f77e0c2324a101d14a58b76e2d4b1
smartkeyerror <smartkeyerror@gmail.com>
2020-12-21 15:18:18 +0800

    :sparkles: daily commit

---
+++ leetcode/bfs/752-Open-the-Lock.cpp
@@ -0,0 +1,17 @@
+
+#include <vector>
+#include <queue>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+/*
+ * 打开一个 4 位数的密码锁，每次操作稚嫩工匠
+ */
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+        
+    }
+};
