d685393ee96c2dcd12d235a74da20e30a1cf773f
smartkeyerror <smartkeyerror@gmail.com>
2021-03-19 13:54:15 +0800

    :construction:

--- leetcode/array/1395-Count-Number-of-Teams.cpp
+++ leetcode/array/1395-Count-Number-of-Teams.cpp
@@ -19,12 +19,15 @@ public:
         // 对每一个 i 我们都去左边儿看看有多少数字比它小，去右边儿看看有多少数字比它大
         for (int i = 1; i < n - 1; i++) {
             int left = 0, right = 0;
+
             for (int j = 0; j < i; j++) {
-                if (rating[j] < rating[i]) left ++;
+                if (rating[j] < rating[i]) 
+                    left ++;
             }
                 
             for (int j = i + 1; j < n; j++) {
-                if (rating[j] > rating[i]) right ++;
+                if (rating[j] > rating[i]) 
+                    right ++;
             }
 
             res += left * right;
@@ -32,10 +35,37 @@ public:
         return res;
     }
 
+    int solution(vector<int>& rating) {
+        int res = 0, n = rating.size();
+
+        for (int i = 1; i < n - 1; i++) {
+            int leftLess = 0, leftLarger = 0, rightLess = 0, rightLarger = 0;
+
+            for (int j = 0; j < i; j++) {
+                if (rating[j] < rating[i]) 
+                    leftLess ++;
+                if (rating[j] > rating[i])
+                    leftLarger ++;
+            }
+                
+            for (int j = i + 1; j < n; j++) {
+                if (rating[j] > rating[i]) 
+                    rightLarger ++;
+                if (rating[j] < rating[i])
+                    rightLess ++;
+            }
+
+            res += leftLess * rightLarger + leftLarger * rightLess;
+        }
+        return res;
+    }
+
     int numTeams(vector<int>& rating) {
-        int r1 = bruteForceSolution(rating);
-        reverse(rating.begin(), rating.end());
-        int r2 = bruteForceSolution(rating);
-        return r1 + r2;
+        // int r1 = bruteForceSolution(rating);
+        // reverse(rating.begin(), rating.end());
+        // int r2 = bruteForceSolution(rating);
+        // return r1 + r2;
+
+        return solution(rating);
     }
 };
