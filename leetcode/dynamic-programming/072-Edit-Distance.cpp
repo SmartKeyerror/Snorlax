3fd59cdfec2f7aec3db737165099b0a28c9a7668
smartkeyerror <smartkeyerror@gmail.com>
2021-04-21 14:35:53 +0800

    :zap:

--- leetcode/dynamic-programming/072-Edit-Distance.cpp
+++ leetcode/dynamic-programming/072-Edit-Distance.cpp
@@ -30,24 +30,22 @@ using namespace std;
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int n1 = word1.size();
-        int n2 = word2.size();
+        int m = word1.size(), n = word2.size();
 
-        vector<vector<int>> memory = vector<vector<int>>(n1+1, vector<int>(n2+1, INT_MAX));
+        vector<vector<int>> dp(m+1, vector<int>(n+1, INT_MAX));
 
-        // 初始化边界条件
-        memory[0][0] = 0;
-        for (int i = 1; i <= n1; i++) memory[i][0] = memory[i-1][0] + 1;
-        for (int j = 1; j <= n2; j++) memory[0][j] = memory[0][j-1] + 1;
-
-        for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
-                if (word1[i-1] == word2[j-1])
-                    memory[i][j] = memory[i-1][j-1];
-                else
-                    memory[i][j] = min(min(memory[i-1][j-1], memory[i][j-1]), memory[i-1][j]) + 1;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0) 
+                    dp[i][j] = j;
+                else if (j == 0) 
+                    dp[i][j] = i;
+                else if (word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else 
+                    dp[i][j] = min(min(dp[i-1][j-1], dp[i][j-1]), dp[i-1][j]) + 1;
             }
         }
-        return memory[n1][n2];
+        return dp[m][n];
     }
 };
