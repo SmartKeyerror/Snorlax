857c6b8d6efcb287de8757607555595f7a0eb313
smartkeyerror <smartkeyerror@gmail.com>
2021-05-31 10:50:44 +0800

    :zap:

--- leetcode/tree/145-Binary-Tree-Postorder-Traversal.cpp
+++ leetcode/tree/145-Binary-Tree-Postorder-Traversal.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -17,6 +18,12 @@ class Solution {
 private:
     vector<int> result;
 
+    vector<int> recursiveSolution(TreeNode *root) {
+        result.clear();
+        postorder(root);
+        return result;
+    }
+
     void postorder(TreeNode *root) {
         if (root == NULL) return;
 
@@ -25,10 +32,38 @@ private:
         result.push_back(root->val);
         return;
     }
+
+    vector<int> stackSolution(TreeNode *root) {
+        if (root == nullptr) return result;
+
+        TreeNode *current = root, *prev = nullptr;
+        stack<TreeNode *> st;
+
+        while (!st.empty() || current != nullptr) {
+            // 首先将所有的左子树入栈
+            while (current != nullptr) {
+                st.push(current);
+                current = current->left;
+            }
+
+            current = st.top();
+
+            if (current->right == nullptr || current->right == prev) {
+                result.push_back(current->val);
+                st.pop();
+                prev = current;
+                current = nullptr;
+            } else {
+                current = current->right;
+            }
+        }
+        return result;
+    }
+
+
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        result.clear();
-        postorder(root);
-        return result;
+        // return recursiveSolution(root);
+        return stackSolution(root);
     }
 };
