cc3660098a3b20df1fff38afe23c12707f3d0f68
smartkeyerror <smartkeyerror@gmail.com>
2021-06-28 15:55:44 +0800

    :art:

--- leetcode/sliding-window/1438-Longest-Continuous-Subarray-With-Absolute-Diff-Less-Than-or-Equal-to-Limit.cpp
+++ leetcode/sliding-window/1438-Longest-Continuous-Subarray-With-Absolute-Diff-Less-Than-or-Equal-to-Limit.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <queue>
 
 using namespace std;
 
@@ -9,6 +10,10 @@ using namespace std;
  * 一道比较典型的滑动窗口问题。在此题中，我们关心的是窗口中的最大值以及最小值，并且我们需要动态的更新窗口中的最大值和最小值。
  * 
  * 不管是使用 multiset 还是 map 来实现，其时间复杂度均为 O(nlogn)，空间复杂度为 O(n)
+ * 
+ * 除此以外，因为我们只对窗口的最大值和最小值感兴趣，所以也可以使用两个单调队列实现: 一个单调递增队列，一个单调递减队列
+ * 
+ * 此时时间复杂度为 O(n)，空间复杂度为 O(n)
  */
 class Solution {
 public:
@@ -52,8 +57,41 @@ public:
         return result;
     }
 
+    int dequeueSolution(vector<int>& nums, int limit) {
+        int result = 0;
+
+        deque<int> maxQueue, minQueue;
+
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            // 维护单调递增队列的性质 => 1, 4, 5, 8
+            while (!maxQueue.empty() && nums[right] > maxQueue.back())
+                maxQueue.pop_back();
+
+            // 维护单调递减队列的性质 => 8, 5, 4, 1
+            while (!minQueue.empty() && nums[right] < minQueue.back())
+                minQueue.pop_back();
+            
+            maxQueue.push_back(nums[right]);
+            minQueue.push_back(nums[right]);
+
+            // 此时不满足题目要求
+            while (!minQueue.empty() && !maxQueue.empty() && maxQueue.front() - minQueue.front() > limit) {
+                if (minQueue.front() == nums[left]) minQueue.pop_front();
+                if (maxQueue.front() == nums[left]) maxQueue.pop_front();
+                left ++;
+            }
+
+            // 此时窗口满足要求，更新结果
+            result = max(result, right - left + 1);
+        }
+        return result;
+    }
+
     int longestSubarray(vector<int>& nums, int limit) {
         // return multisetSolution(nums, limit);
-        return mapSolution(nums, limit);
+        // return mapSolution(nums, limit);
+        return dequeueSolution(nums, limit);
     }
 };
