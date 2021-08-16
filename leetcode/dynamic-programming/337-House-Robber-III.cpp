c3e8e4d89630394f6f6751764532ea5eb11a26c9
smartkeyerror <smartkeyerror@gmail.com>
2021-08-16 15:11:11 +0800

    :art:

--- leetcode/dynamic-programming/337-House-Robber-III.cpp
+++ leetcode/dynamic-programming/337-House-Robber-III.cpp
@@ -16,51 +16,45 @@ struct TreeNode {
  * 打家劫舍终极版: 二叉树中洗劫
  * 
  * 在一棵二叉树中进行洗劫，不能洗劫直接相邻的节点，也就是父子节点，兄弟节点可同时洗劫。
- * 
- * House Robber 问题本质上仍是0-1背包问题，即对于当前的节点，可以选择洗劫，也可以选择不洗劫。那么我们如何在一棵树中来完成洗劫?
- * 
+ *  
  *          a
  *        /   \
  *       b     c
  *      / \   / \
  *     d   e f   g
  * 
- * 假设我们的二叉树长这个样子，由于我们不能偷相邻的两个节点，也就是说， 假如我们偷了节点 a (root节点) 的话，那么节点 b (左孩子节点) 和节点 c (右孩子节点) 便不能再偷了，只能在 d、e、f、g 这4个节点中选择
- * 相反，如果我们不去偷节点 a 的话，那么节点 b 和节点 c 我们都能偷。
+ * 假设 f(n) 表示偷当前节点的最大收益，g(n) 表示不偷当前节点的最大收益，显而易见地我们可以得到 f(n) 的结果:
+ *      f(n) = n->val + g(n->left) + g(n-right)
  * 
- * 如此一来，我们的状态转移方程就很清晰了: f(node) = max(V(node)+f(node.left.all)+f(node.right.all), f(node.left)+f(node.right))
+ * 如果说我们不偷当前节点的话，那么就可以去左、右两棵子树中寻找最大收益。对于左子树，存在两种情况，对于右子树，也存在两种情况，则有:
+ *      g(n) = max{ max{f(n->left), g(n->left)}, max{f(n->right), g(n->right)} }     
  * 
- * 简单来说就是偷了当前根节点，那么只能去左右子节点的子节点进行偷取，否则，偷取左右子节点。
  */
+
+struct Status {
+    int selected;
+    int notselected; 
+};
+
 class Solution {
 private:
-    
-    unordered_map<TreeNode *, int> memory;
-    
-    int dfs(TreeNode *root) {
-        if (root == nullptr) return 0;
-        
-        if (memory.find(root) != memory.end()) 
-            return memory[root];
-        
-        int value = 0;
-        
-        if (root->left)
-            value += dfs(root->left->right) + dfs(root->left->right);
+    Status dfs(TreeNode* root) {
+        if (root == nullptr) return {0, 0};
 
-        if (root->right)
-            value += dfs(root->right->left) + dfs(root->right->right);
-        
-        // 在选择偷当前根节点和不偷当前根节点之间进行比较
-        int maxValue = max(root->val + value, dfs(root->left) + dfs(root->right));
+        Status left = dfs(root->left);
+        Status right = dfs(root->right);
 
-        memory[root] = maxValue;
+        int selected = root->val + left.notselected + right.notselected;
+        int notselected = max(max(left.selected, left.notselected), max(right.selected, right.notselected));
 
-        return maxValue;
+        return {selected, notselected};
     }
-
 public:
     int rob(TreeNode* root) {
-        return dfs(root);
+        if (root == nullptr) return 0;
+
+        Status res = dfs(root);
+
+        return max(res.selected, res.notselected);
     }
 };
