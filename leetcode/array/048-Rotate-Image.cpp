a3d1784f9a46b5d696f2eef0ea2102473a2e4f0c
smartkeyerror <smartkeyerror@gmail.com>
2021-08-16 19:20:25 +0800

    :art:

--- leetcode/array/048-Rotate-Image.cpp
+++ leetcode/array/048-Rotate-Image.cpp
@@ -6,22 +6,51 @@ using namespace std;
 /*
  * 顺时针旋转图像 90°，在日常的图片预览等应用中使用非常频繁。
  * 
- * 这里是顺时针旋转 90°，其实我们可以旋转 45°，25° 等等等等。熟悉 Python 的小伙伴儿可以看看 pillow 库中的实现
+ * 5  1  9  11
+ * 2  4  8  10
+ * 13 3  6  7
+ * 15 14 12 16
  * 
- * from PIL import Image
+ * 将会得到 
  * 
- * image = Image.open("avatar.jpg")
- * image_rotate = image.rotate(45)
- * image_rotate = image.rotate(-45)
+ * 15 12  2  5
+ * 14  3  4  1
+ * 12  6  8  9
+ * 16  7 10  11
+ * 
+ * 第一行的元素会出现在最后一列，并且每一列的行数和原来的列数是相等的。第二行的元素会出现在倒数第二列，并且每一列的行数和原来的列数是相等的。
+ * 
+ * 所以假设我们有一个辅助数组 new_matrix 的话，就有 matrix[row][col] = new_matrix[col][n-1-row]。
+ * 
+ * 这个问题我们可以先水平翻转，然后再基于主对角线翻转。水平翻转其实就是把最后一行和第一行置换，倒数第二行和第二行置换:
+ * 
+ * matrix[row][col] => matrix[n-1-row][col]
+ * 
+ * 主对角线翻转其实就是将原来的行变成列，将原来的列变成行:
+ * 
+ * matrix[row][col] => matrix[col][row]
+ * 
+ * 最终有:
+ * 
+ * matrix[row][col] => matrix[n-1-row][col] => matrix[col][n-1-row]，也就得到我们最开始的那个公式
  */
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        for (int i = 0; i < matrix.size(); i++)
-            for (int j = 0; j < matrix[0].size(); j++)
+        int n = matrix.size();
+
+        // 水平翻转,matrix[row][col] = matrix[n-1-row][col]
+        for (int i = 0; i < n/2; i++) {
+            for (int j = 0; j < n; j++) {
+                swap(matrix[i][j], matrix[n-1-i][j]);
+            }
+        }
+
+        // 基于主对角线翻转, matrix[row][col] => matrix[col][row]
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
                 swap(matrix[i][j], matrix[j][i]);
-        
-        for (int i = 0; i < matrix.size(); i++)
-            reverse(matrix[i].begin(), matrix[i].end());
+            }
+        }
     }
 };
